1e44f5f03abf56f85600949793464a376cd3cb70
Wecreds <Wecredsbr@gmail.com>
2023-06-28 14:53:35 -0300

    Aula 14.

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivrosViewSet
 
 router = DefaultRouter()
@@ -14,4 +20,6 @@ router.register(r"livros", LivrosViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
